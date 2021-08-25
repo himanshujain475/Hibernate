5fe553180fc33e1b4a1d908e48a9305b392d9584
Himanshu jain <himanshujain475@gmail.com>
2021-08-26 02:19:14 +0530

    adding

---
+++ Hibernate/src/main/java/himanshu/practice/App.java
@@ -0,0 +1,13 @@
+package himanshu.practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
